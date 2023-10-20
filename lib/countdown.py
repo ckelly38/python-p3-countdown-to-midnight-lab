26aa026fe87e93d42f2210f3f23b40973664aea1
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 16:54:14 -0600

    this passes the tests and adds a simple count down method and calls sleep() from python's time module

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
-# your code goes here!
+import time;
+def maincountdown(num, usesleep=False):
+    if (num == None): raise Exception("num must be an integer, but it was not defined!");
+    elif (type(num) == int): pass;
+    else: raise Exception("num must be an integer, but it was not defined!");
+    while (num > 0):
+        print(f"{num} SECOND(S)!");
+        if (usesleep): time.sleep(1);
+        else: pass;
+        num -= 1;
+    print("HAPPY NEW YEAR!");
+    return None;
+
+def countdown(num):
+    return maincountdown(num);
+
+def countdown_with_sleep(num):
+    return maincountdown(num, True);
+
+#countdown_with_sleep(10);
